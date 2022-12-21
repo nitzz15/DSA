8e9bc391ffa06065603b58dfe0947fd18f87602b
exam <exam@DAC16.eserver.com>
2022-12-21 11:07:38 +0530

    updated

--- HashSet/src/com/Set/HashSetTest.java
+++ HashSet/src/com/Set/HashSetTest.java
@@ -7,6 +7,7 @@ public class HashSetTest {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		System.out.println("hello");
 		HashSet<Integer> set = new HashSet<>();
 		
 		set.add(12);
