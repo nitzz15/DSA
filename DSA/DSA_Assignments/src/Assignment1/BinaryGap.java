3d7d550eeaac4992953bc4eccde80acba7eb9d47
Aish <asolkaraishwarya@gmail.com>
2022-12-21 11:10:46 +0530

    Comit by ash

--- DSA/DSA_Assignments/src/Assignment1/BinaryGap.java
+++ DSA/DSA_Assignments/src/Assignment1/BinaryGap.java
@@ -12,7 +12,7 @@ public class BinaryGap {
 	System.out.println(binary);	
 	
 	System.out.println("hello Nitesh KFC la party kdhi denar ahes");
-	System.out.println("HELLO NITZZZZZ");
+	System.out.println("HELLO Ash");
 	
 		ArrayList<Integer> Binarylist = new ArrayList<Integer>();
 		for(int i=0;i<binary.length();i++) {
